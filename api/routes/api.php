654a3b0f32fe9f236c838804f27f5aef34d12245
Kenneth Fernandez <kennethworks26@gmail.com>
2020-10-09 18:45:13 +0800

    feat(auth): getting authenticated user information with Fractal

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth', 'namespace' => 'App\Http\Controllers\Auth'], function () {
     Route::post('/login', 'LoginController');
+    Route::get('/me', 'MeController');
 });
