93e652a6675077e485d34314581b883a288a98f7
Kenneth Fernandez <kennethworks26@gmail.com>
2020-10-23 03:09:33 +0800

    feat(search): add search, stats, results instantsearch components, ssr setup

---
+++ client/pages/search/components/SearchSnippetCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <SnippetCard :snippet="snippet">
+    <nuxt-link :to="{}">{{ snippet.author.data.name }}</nuxt-link>
+  </SnippetCard>
+</template>
+<script>
+import SnippetCard from "@/components/snippets/SnippetCard";
+
+export default {
+  props: {
+    snippet: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
--- client/pages/search/index.vue
+++ client/pages/search/index.vue
@@ -1,45 +1,66 @@
 <template>
-  <div>
-    <div class="bg-white mb-16">
-      <div class="container py-10 pb-16">
-        <h1 class="text-4xl text-gray-700 font-medium leading-tight mb-4">
-          Search
-        </h1>
+  <AisInstantSearchSsr>
+    <div>
+      <div class="bg-white mb-16">
+        <div class="container py-10 pb-16">
+          <div class="flex justify-between items-center mb-6">
+            <h1 class="text-4xl text-gray-700 font-medium leading-tight mb-4">
+              Search
+            </h1>
+            <client-only>
+              <AisPoweredBy />
+            </client-only>
+          </div>
+
+          <AisSearchBox
+            placeholder="Search snippets"
+            :class-names="{
+              'ais-SearchBox': 'w-full',
+              'ais-SearchBox-input': 'w-full border-2 border-gray-400 rounded-lg block p-4 text-lg focus:outline-none focus:border-blue-500',
+              'ais-SearchBox-submit': 'hidden',
+              'ais-SearchBox-submitIcon': 'hidden',
+              'ais-SearchBox-reset': 'hidden'
+            }"
+          />
+        </div>
+      </div>
+
+      <div class="container pb-16">
+        <AisStateResults>
+          <div slot-scope="{query}">
+            <template v-if="query.length">
+              <AisStats>
+                <h1
+                  class="text-xl text-gray-600 font-medium mb-6"
+                  slot-scope="{ nbHits }"
+                >Snippets ({{ nbHits }})</h1>
+              </AisStats>
+              <AisHits>
+                <div
+                  slot="item"
+                  slot-scope="{item}"
+                >
+                  <SearchSnippetCard :snippet="item.data" />
+                </div>
+              </AisHits>
+            </template>
+          </div>
+        </AisStateResults>
       </div>
     </div>
-
-    <div class="container">
-      <h1 class="text-xl text-gray-600 font-medium mb-6">Snippets (x)</h1>
-
-      <ais-instant-search-ssr>
-        <ais-search-box class="mb-10" />
-        <ais-stats class="mb-6" />
-        <ais-refinement-list attribute="brand" />
-        <ais-hits class="mb-10">
-          <template slot="item" slot-scope="{ item }">
-            <p>
-              <ais-highlight attribute="data.title" :hit="item" />
-            </p>
-            <p>
-              <ais-highlight attribute="data.author" :hit="item" />
-            </p>
-          </template>
-        </ais-hits>
-        <ais-pagination />
-      </ais-instant-search-ssr>
-    </div>
-  </div>
+  </AisInstantSearchSsr>
 </template>
 
 <script>
+import SearchSnippetCard from "./components/SearchSnippetCard"
 import {
   AisInstantSearchSsr,
-  AisRefinementList,
+  AisStateResults,
   AisHits,
-  AisHighlight,
   AisSearchBox,
   AisStats,
   AisPagination,
+  AisPoweredBy,
   createServerRootMixin,
 } from "vue-instantsearch";
 import algoliasearch from "algoliasearch/lite";
@@ -49,40 +70,86 @@ const searchClient = algoliasearch(
   process.env.ALGOLIA_SECRET
 );
 
+function nuxtRouter (vueRouter) {
+  return {
+    read () {
+      return vueRouter.currentRoute.query;
+    },
+    write (routeState) {
+      vueRouter.push({
+        query: routeState,
+      });
+    },
+    createURL (routeState) {
+      return vueRouter.resolve({
+        query: routeState,
+      }).href;
+    },
+    onUpdate (cb) {
+      if (typeof window === 'undefined') return;
+
+      this._onPopState = event => {
+        const routeState = event.state;
+        // On initial load, the state is read from the URL without
+        // update. Therefore, the state object isn't present. In this
+        // case, we fallback and read the URL.
+        if (!routeState) {
+          cb(this.read());
+        } else {
+          cb(routeState);
+        }
+      };
+      window.addEventListener('popstate', this._onPopState);
+    },
+    dispose () {
+      if (typeof window === 'undefined') return;
+
+      window.removeEventListener('popstate', this._onPopState);
+    },
+  };
+}
+
 export default {
   components: {
+    SearchSnippetCard,
     AisInstantSearchSsr,
-    AisRefinementList,
+    AisStateResults,
     AisHits,
-    AisHighlight,
     AisSearchBox,
     AisStats,
     AisPagination,
+    AisPoweredBy,
   },
-  head() {
+  head () {
     return {
       title: "Search",
-      link: [
-        {
-          rel: "stylesheet",
-          href:
-            "https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/algolia-min.css",
-        },
-      ],
     };
   },
-  mixins: [
-    createServerRootMixin({
+  data () {
+    // Create it in `data` to access the Vue Router
+    const mixin = createServerRootMixin({
       searchClient,
-      indexName: "snippets",
-    }),
-  ],
-  serverPrefetch() {
-    return this.instantsearch.findResultsState(this).then((algoliaState) => {
+      indexName: 'snippets',
+      routing: {
+        router: nuxtRouter(this.$nuxt.$router),
+      },
+    });
+    return {
+      ...mixin.data(),
+    };
+  },
+  provide () {
+    return {
+      // Provide the InstantSearch instance for SSR
+      $_ais_ssrInstantSearchInstance: this.instantsearch,
+    };
+  },
+  serverPrefetch () {
+    return this.instantsearch.findResultsState(this).then(algoliaState => {
       this.$ssrContext.nuxt.algoliaState = algoliaState;
     });
   },
-  beforeMount() {
+  beforeMount () {
     const results =
       this.$nuxt.context.nuxtState.algoliaState || window.__NUXT__.algoliaState;
 
